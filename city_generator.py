5bee6a9dbd6cfcd86783cf1a294b9d5b0a6aa036
Unknown <sam.masaki@gmail.com>
2019-05-24 14:43:06 -0700

    Shortened lines

--- city_generator.py
+++ city_generator.py
@@ -42,7 +42,8 @@ def main():
     pygame.init()
     drawing.init()
 
-    screen_data = drawing.ScreenData(pygame.display.set_mode(config.SCREEN_RES, pygame.RESIZABLE), (0, 0))
+    screen_data = drawing.ScreenData(
+        pygame.display.set_mode(config.SCREEN_RES, pygame.RESIZABLE), (0, 0))
     input_data = InputData()
     path_data = pathing.PathData()
     selection = None
@@ -52,8 +53,6 @@ def main():
     for road in city.roads:
         city_labels.append((str(road.global_id),
                             road.point_at(0.5)))
-
-    # Inter-frame Variables
 
     prev_time = pygame.time.get_ticks()
 
@@ -72,7 +71,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.VIDEORESIZE:
-                screen_data.screen = pygame.display.set_mode(event.dict["size"], pygame.RESIZABLE)
+                screen_data.screen = pygame.display.set_mode(
+                    event.dict["size"], pygame.RESIZABLE)
                 config.SCREEN_RES = event.dict["size"]
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_g:
@@ -86,9 +86,11 @@ def main():
                                             road.point_at(0.5)))
                 # Pathing
                 elif event.key == pygame.K_z:
-                    path_data.start = road_near_point(drawing.screen_to_world(input_data.pos, screen_data.pan, screen_data.zoom), city)
+                    path_data.start = road_near_point(input_data.pos,
+                                                      screen_data, city)
                 elif event.key == pygame.K_x:
-                    path_data.end = road_near_point(drawing.screen_to_world(input_data.pos, screen_data.pan, screen_data.zoom), city)
+                    path_data.end = road_near_point(input_data.pos,
+                                                    screen_data, city)
                 elif event.key == pygame.K_c:
                     pathing.astar(path_data, city.roads)
                 elif event.key == pygame.K_v:
@@ -102,19 +104,23 @@ def main():
                 elif event.button == 5:
                     screen_data.zoom_out(input_data.pos)
 
-        # Dragging
+        # Dragging & Selection
         if input_data.prev_pressed[0]:
-            if input_data.pressed[0]:
-                screen_data.pan = vectors.add(screen_data.pan, vectors.sub(input_data.pos, input_data.drag_prev_pos))
+            if input_data.pressed[0]:  # Continue drag
+                screen_data.pan = vectors.add(
+                    screen_data.pan,
+                    vectors.sub(input_data.pos, input_data.drag_prev_pos))
                 input_data.drag_prev_pos = input_data.pos
             else:
-                if input_data.pos == input_data.drag_start:
-                    selection = debug.selection_from_road(road_near_point(drawing.screen_to_world(input_data.drag_start, screen_data.pan, screen_data.zoom), city))
-
+                if input_data.pos == input_data.drag_start:  # Select road
+                    selection = debug.selection_from_road(
+                        road_near_point(input_data.drag_start,
+                                        screen_data, city))
+                # Clear out drag information
                 input_data.drag_start = None
                 input_data.drag_prev_pos = (0, 0)
         else:
-            if input_data.pressed[0]:
+            if input_data.pressed[0]:  # Drag started
                 input_data.drag_start = input_data.pos
                 input_data.drag_prev_pos = input_data.pos
 
@@ -123,32 +129,46 @@ def main():
             drawing.draw_heatmap(50, city, screen_data)
         if debug.SHOW_SECTORS:
             drawing.draw_sectors(screen_data)
+
+        # Draw roads
         if debug.SHOW_ISOLATE_SECTOR and selection is not None:
             for sector in sectors.from_seg(selection.road):
                 drawing.draw_all_roads(city.sectors[sector], screen_data)
         elif debug.SHOW_MOUSE_SECTOR:
-            mouse_sec = sectors.containing_sector(drawing.screen_to_world(input_data.pos, screen_data.pan, screen_data.zoom))
+            mouse_sec = sectors.containing_sector(
+                drawing.screen_to_world(input_data.pos,
+                                        screen_data.pan, screen_data.zoom))
             if mouse_sec in city.sectors:
                 drawing.draw_all_roads(city.sectors[mouse_sec], screen_data)
         else:
-            tl_sect = sectors.containing_sector(drawing.screen_to_world((0, 0), screen_data.pan, screen_data.zoom))
-            br_sect = sectors.containing_sector(drawing.screen_to_world(config.SCREEN_RES, screen_data.pan, screen_data.zoom))
+            tl_sect = sectors.containing_sector(
+                drawing.screen_to_world((0, 0),
+                                        screen_data.pan, screen_data.zoom))
+            br_sect = sectors.containing_sector(
+                drawing.screen_to_world(config.SCREEN_RES,
+                                        screen_data.pan, screen_data.zoom))
             for x in range(tl_sect[0], br_sect[0] + 1):
                 for y in range(tl_sect[1], br_sect[1] + 1):
                     if (x, y) in city.sectors:
-                        drawing.draw_all_roads(city.sectors[(x, y)], screen_data)
+                        drawing.draw_all_roads(city.sectors[(x, y)],
+                                               screen_data)
 
         drawing.draw_roads_selected(selection, screen_data)
         drawing.draw_roads_path(path_data, screen_data)
 
         if debug.SHOW_INFO:
-            debug_labels = debug.labels(screen_data, input_data, path_data, selection, city)
+            debug_labels = debug.labels(screen_data, input_data,
+                                        path_data, selection, city)
 
             for x in range(len(debug_labels[0])):
-                drawing.draw_label_screen((debug_labels[0][x], (10, 10 + x * 15)), screen_data, 1)
+                label_pos = (10, 10 + x * 15)
+                drawing.draw_label_screen((debug_labels[0][x], label_pos),
+                                          screen_data, 1)
 
             for x in range(len(debug_labels[1])):
-                drawing.draw_label_screen((debug_labels[1][x], (config.SCREEN_RES[0] - 10, 10 + x * 15)), screen_data, -1)
+                label_pos = (config.SCREEN_RES[0] - 10, 10 + x * 15)
+                drawing.draw_label_screen((debug_labels[1][x], label_pos),
+                                          screen_data, -1)
 
         if debug.SHOW_ROAD_ORDER:
             for label in city_labels:
@@ -179,13 +199,19 @@ def handle_keys_debug(key):
         debug.SHOW_MOUSE_SECTOR = not debug.SHOW_MOUSE_SECTOR
 
 
-def road_near_point(world_pos: Tuple[float, float], city: generation.City) -> Optional[roads.Segment]:
+def road_near_point(screen_pos: Tuple[float, float],
+                    screen_data: 'drawing.ScreenData',
+                    city: generation.City) -> Optional[roads.Segment]:
     """
     Gets the closest road to the given point within 100 units
-    :param world_pos: The world-position to look around
+    :param screen_pos: The screen-position to look around
+    :param screen_data: The current state of the view port
     :param city: A city with sectors and roads to search
-    :return: The nearest road if there is one within 100 units of the point, None otherwise
+    :return: The nearest road if there is one within 100 units of the point,
+    None otherwise
     """
+    world_pos = drawing.screen_to_world(screen_pos, screen_data.pan,
+                                        screen_data.zoom)
     closest: Tuple[roads.Segment, float] = (None, 9999)
     found_road = None
     examine_sectors = sectors.from_point(world_pos, 100)
@@ -200,9 +226,6 @@ def road_near_point(world_pos: Tuple[float, float], city: generation.City) -> Op
                 found_road = closest[0]
 
     return found_road
-
-
-
 
 
 if __name__ == "__main__":
